573ed54e95db7970b09827699feee4aa34c2c16e
dennisyd <dennisyd@alum.mit.edu>
2023-01-06 17:44:16 +0000

    fn to get n dates m days away

--- src/util.ts
+++ src/util.ts
@@ -16,37 +16,3 @@ export const getDates = () => {
     };
   });
 };
-
-export const BuildSkyscannerRequests = () => {
-  let dates = getDates();
-
-  // for each route, create a request with each date
-  return routes.map((el) => {
-    let res = [];
-
-    for (let i = 0; i < dates.length; i++) {
-      console.log(el, dates[i]);
-
-      res.push({
-        // hardcoded lots of values to minimize combinations and requests to skyscanner, because we're using the public API which is heavily rate limited
-        market: 'US',
-        locale: 'en-US',
-        currency: 'USD',
-        query_legs: [
-          {
-            origin_place_id: {
-              iata: el['origin'],
-            },
-            destination_place_id: {
-              iata: el['destination'],
-            },
-            date: dates[i],
-          },
-        ],
-        cabinclass: 'CABIN_CLASS_ECONOMY',
-        adults: '1',
-      });
-    }
-    return res;
-  });
-};
