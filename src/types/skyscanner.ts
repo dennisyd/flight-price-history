804151143987896da2d6a9ce56b5c766b92b83b4
dennisyd <dennisyd@alum.mit.edu>
2023-01-06 21:11:38 +0000

    1 missing underscore = 30 min time wasted :D

--- src/types/skyscanner.ts
+++ src/types/skyscanner.ts
@@ -1,20 +1,22 @@
 export default interface SkyscannerAPISearchCreate {
-  market: string;
-  locale: string;
-  currency: string;
-  query_legs: {
-    origin_place_id: {
-      iata: string;
-    };
-    destination_place_id: {
-      iata: string;
-    };
-    date: {
-      year: number;
-      month: number;
-      day: number;
-    };
-  }[];
-  cabinclass: string;
-  adults: string;
+  query: {
+    market: string;
+    locale: string;
+    currency: string;
+    query_legs: {
+      origin_place_id: {
+        iata: string;
+      };
+      destination_place_id: {
+        iata: string;
+      };
+      date: {
+        year: number;
+        month: number;
+        day: number;
+      };
+    }[];
+    adults: string;
+    cabin_class: string;
+  };
 }
