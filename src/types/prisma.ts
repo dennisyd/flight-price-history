65784b1523831bc63237a401814f13b7650cbd7d
dennisyd <dennisyd@alum.mit.edu>
2023-01-08 05:39:02 +0000

    types

---
+++ src/types/prisma.ts
@@ -0,0 +1,11 @@
+export interface SevendaylinesRowNoId {
+  routeid: number;
+  date: string;
+  price: number;
+}
+
+export interface ThirtydaylinesRowNoId {
+  routeid: number;
+  date: string;
+  price: number;
+}
