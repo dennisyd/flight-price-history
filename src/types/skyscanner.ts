0188a0e4f1c666751ad4985991433cd23c54053a
dennisyd <dennisyd@alum.mit.edu>
2023-01-08 05:41:32 +0000

    update type to include metadata forward

--- src/types/skyscanner.ts
+++ src/types/skyscanner.ts
@@ -1,22 +1,28 @@
-export default interface SkyscannerAPISearchCreate {
-  query: {
-    market: string;
-    locale: string;
-    currency: string;
-    query_legs: {
-      origin_place_id: {
-        iata: string;
-      };
-      destination_place_id: {
-        iata: string;
-      };
-      date: {
-        year: number;
-        month: number;
-        day: number;
-      };
-    }[];
-    adults: string;
-    cabin_class: string;
+export default interface SkyscannerAPICreate {
+  metadata: {
+    prismaRouteId: number;
+    date: string;
+  };
+  body: {
+    query: {
+      market: string;
+      locale: string;
+      currency: string;
+      query_legs: {
+        origin_place_id: {
+          iata: string;
+        };
+        destination_place_id: {
+          iata: string;
+        };
+        date: {
+          year: number;
+          month: number;
+          day: number;
+        };
+      }[];
+      adults: string;
+      cabin_class: string;
+    };
   };
 }
