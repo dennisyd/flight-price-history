be114bbfcbb3efaa24f782485eca20b26f761930
dennisyd <dennisyd@alum.mit.edu>
2023-01-08 05:39:17 +0000

    create new database routes

--- src/prismaapi.ts
+++ src/prismaapi.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, trackedroutes } from '@prisma/client';
+import { SevendaylinesRowNoId, ThirtydaylinesRowNoId } from './types/prisma';
 
 const prisma = new PrismaClient();
 
@@ -11,3 +12,24 @@ export const getAllTrackedroutes = async (): Promise<trackedroutes[]> => {
   }
 };
 
+export const createSevendayChart = async (data: SevendaylinesRowNoId[]) => {
+  try {
+    return await prisma.sevendaylines.createMany({
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const createThirtydayChart = async (data: ThirtydaylinesRowNoId[]) => {
+  try {
+    return await prisma.thirtydaylines.createMany({
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
