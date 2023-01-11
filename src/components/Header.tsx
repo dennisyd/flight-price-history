b352f0f06312b743a25022c28fa934aafc2ebcbc
dennisyd <dennisyd@alum.mit.edu>
2023-01-11 00:07:49 +0000

    extract header into seperate component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from 'react';
 import { Container } from './Container';
 import { parentPort } from 'worker_threads';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Prisma, PrismaClient, prisma } from '@prisma/client';
 
 export function Header({ showResultsList }: any) {
   const [inputText, setInputText] = useState('');
+  const router = useRouter();
 
   const handleSearch = (e: any) => {
+    // TODO: add validation on search input
     if (e.key === 'Enter') {
-      showResultsList(e.target.value);
+      const origin = inputText.substring(0, 3);
+      const destination = inputText.substring(inputText.length - 3);
+      router.push(`/search/?origin=${origin}&destination=${destination}`);
     }
   };
 
+  // TODO: extract searchbar into its own component
   return (
     <header>
       <nav>
@@ -18,9 +26,11 @@ export function Header({ showResultsList }: any) {
           <div className="navbar bg-base-100">
             <div className="navbar-start">
               <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">
-                  flightfarehist
-                </a>
+                <Link href="/">
+                  <a className="btn btn-ghost normal-case text-xl">
+                    flightfarehist
+                  </a>
+                </Link>
               </div>
             </div>
             <div className="navbar-center">
@@ -50,16 +60,17 @@ export function Header({ showResultsList }: any) {
 }
 
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts
-  const routes = await fetch('http://localhost:3000/api/trackedroutes').then(
-    (res) => res.json()
-  );
-
-  console.log('routes: ', routes);
+  const prisma = new PrismaClient();
+  // this looks inefficient but it's perfectly fine, these tables will always be very small
+  const routes = await prisma.trackedroutes.findMany();
+  const sevendaylines = await prisma.sevendaylines.findMany();
+  const thirtydaylines = await prisma.thirtydaylines.findMany();
 
   return {
     props: {
       routes,
+      sevendaylines,
+      thirtydaylines,
     },
   };
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,10 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { Header } from '../components/Header';
 import { Container } from '../components/Container';
 import { PrismaClient } from '@prisma/client';
 import { useEffect, useState } from 'react';
-import { Graph } from '../components/Graph';
 
 const Home: NextPage = ({ routes, sevendaylines, thirtydaylines }: any) => {
-  const [showGraph, setShowGraph] = useState(false);
-  const [route, setRoute] = useState({
-    origin: '',
-    destination: '',
-  });
-
-  const showResultsList = (inputText: any) => {
-    const origin = inputText.substring(0, 3);
-    const destination = inputText.substring(inputText.length - 3);
-    setRoute({ origin, destination });
-    setShowGraph(true);
-  };
-
-  useEffect(() => {
-    console.log('useEffect in index.tsx');
-    // console.log('routes: ', routes);
-    // console.log('sevendaylines: ', sevendaylines);
-    // console.log('thirtydaylines: ', thirtydaylines);
-    console.log('route: ', route);
-  });
-
   return (
     <div>
       <Head>
@@ -36,32 +13,26 @@ const Home: NextPage = ({ routes, sevendaylines, thirtydaylines }: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header routes={routes} showResultsList={showResultsList} />
-
       <main>
         <Container>
-          {showGraph && (
-            <Graph route={route} datapoints={[sevendaylines, thirtydaylines]} />
-          )}
+          use the search bar above with the format "[origin] to [destination]"
+          (e.g. "LGW to BOS") to see the history of flight fares 
+          <br />
+          <br />
+          our data is provided by{' '}
+          <a className="underline" href="http://skyscanner.com">
+            skyscanner
+          </a>
+          <br />
+          <br />
+          made by{' '}
+          <a className="underline" href="http://github.com/eula01">
+            eula01
+          </a>
         </Container>
       </main>
     </div>
   );
 };
-
-export async function getStaticProps() {
-  const prisma = new PrismaClient();
-  const routes = await prisma.trackedroutes.findMany();
-  const sevendaylines = await prisma.sevendaylines.findMany();
-  const thirtydaylines = await prisma.thirtydaylines.findMany();
-
-  return {
-    props: {
-      routes,
-      sevendaylines,
-      thirtydaylines,
-    },
-  };
-}
 
 export default Home;
